acb974eba7c2461f641a671210e0e3ad58617fbe
Ward Nelson <wnelson@nvidia.com>
2025-02-27 13:19:44 -0600

    add aoc02

---
+++ aoc02/target/debug/aoc02.d
@@ -0,0 +1,1 @@
+/home/wnelson/src/rust/aoc24_rust/aoc02/target/debug/aoc02: /home/wnelson/src/rust/aoc24_rust/aoc02/src/main.rs
---
+++ aoc02/target/debug/deps/aoc02-3919109bc24ea8d7.d
@@ -0,0 +1,5 @@
+/home/wnelson/src/rust/aoc24_rust/aoc02/target/debug/deps/aoc02-3919109bc24ea8d7: src/main.rs
+
+/home/wnelson/src/rust/aoc24_rust/aoc02/target/debug/deps/aoc02-3919109bc24ea8d7.d: src/main.rs
+
+src/main.rs:
